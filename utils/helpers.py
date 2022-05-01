592e245997954741342d436c6baa918f7ee0fce8
“veronica <molcanova.v_qschool@qsoft.ru>
2022-05-02 01:14:24 +0300

    005 / поиск решений по заявке

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,5 @@
+import re
+
 def send_list(data) -> str:
     out = ''
     for key in data.keys():
@@ -14,3 +16,16 @@ def split_into_fullname(id: int, message: str, is_admin: bool) -> dict:
         'middle_name': data[2],
         'admin': is_admin,
     }
+
+# def _pretty_steps(steps) -> list:
+#     steps_text = ''
+#     for i in re.split('\n', steps):
+#         steps_text += i.strip() + '\n'
+#     return steps_text
+
+def pretty_output(response) -> str:
+    text = ''
+    for res in response:
+        text += f"<b>{res['description']}</b>\n{res['steps']}\n"
+
+    return text
